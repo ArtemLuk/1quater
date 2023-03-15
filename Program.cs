a80e62b6c85bb6a9ae5a81093cf2d598f5a65c92
Artem Lukashov <artem.lukashov1@gmail.com>
2023-03-15 08:44:47 +0100

    Program edited, added diff examples

--- Program.cs
+++ Program.cs
@@ -1,5 +1,14 @@
-﻿
-string[] GetArray(int N){
+﻿//Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+//длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
+//либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
+//лучше обойтись исключительно массивами.
+
+//Примеры:
+//[“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+//[“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+//[“Russia”, “Denmark”, “Kazan”] → []
+
+string[] GetArray(int N){    //Ввод элементов массива с клавиатуры
     string [] arr = new string [N];
     for (int i=0; i<N; i++){
         Console.WriteLine($"Pleasee enter {i+1} string: ");
@@ -8,28 +17,25 @@ string[] GetArray(int N){
     return arr;
 }
 
-string[] NewArray(string[] array, int maxLength){
+string[] NewArray(string[] array, int maxLength){   //Поиск размера нового массива и его заполнение
     int i = 0;
     foreach (string el in array)
         if (el.Length <= maxLength)
             i++;
-
     string[] newarray = new string[i];
     i = 0;
     foreach (string el in array)
-        if (el.Length <= maxLength)
-        {
+        if (el.Length <= maxLength){
             newarray[i] = el;
             i++;
         }
     return newarray;
 }
 
-void Printarray(string [] array){
+void Printarray(string [] array){  // Вывод массива строк в консоль
     Console.Write("[");
     foreach (string el in array)
         Console.Write(el + ", ");
-    
 Console.Write("]");
 }
 
@@ -38,7 +44,10 @@ Console.WriteLine("Please the quantity of strings: ");
 int a = int.Parse(Console.ReadLine());
 
 
-string [] A = GetArray(3);
+string [] A = GetArray(a);
+//string [] A = new string[] {"Hello", "2", "world", ":-)"};
+//string [] A = new string[] {"1234", "1567", "-2", "computer science"};
+//string [] A = new string[] {"Russia", "Denmark", "Kazan"};
 Console.WriteLine("Current array is: ");
 Printarray(A);
 Console.WriteLine();
